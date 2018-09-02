ccfbb779709f6ee7186372320844de21526e2710
ocramz <zocca.marco@gmail.com>
2018-09-02 12:21:17 +0200

    add pointer_symbol parsers

--- wordnet31/src/NLP/WordNet31/Parsers.hs
+++ wordnet31/src/NLP/WordNet31/Parsers.hs
@@ -21,10 +21,6 @@ Each index file is an alphabetized list of all the words found in WordNet in the
 A data file for a syntactic category contains information corresponding to the synsets that were specified in the lexicographer files, with relational pointers resolved to synset_offset s. Each line corresponds to a synset. Pointers are followed and hierarchies traversed by moving from one synset to another via the synset_offset s.
 
 
-
--- data.noun "compliance" :
-
-0101 + 02548492 v 0101 ! 01182197 n 0101 ~ 01169875 n 0000 | the act of obeying; dutiful or submissive behavior with respect to another person
 -}
 
 type Lemma = T.Text
@@ -32,18 +28,145 @@ type Lemma = T.Text
 lemma :: AL.Parser Lemma
 lemma = A.takeWhile (A.inClass "a-z_")
 
-data Pos = Noun | Verb | Adj | Adv deriving (Eq, Show, Ord)
+-- data Pos = Noun | Verb | Adj | Adv deriving (Eq, Show, Ord)
 
-noun :: A.Parser Pos
-noun =
-  (Noun <$ A.char 'n') <|>
-  (Verb <$ A.char 'v') <|>
-  (Adj <$ A.char 'a') <|>
-  (Adv <$ A.char 'r')
+-- noun :: A.Parser Pos
+-- noun =
+--   (Noun <$ A.char 'n') <|>
+--   (Verb <$ A.char 'v') <|>
+--   (Adj <$ A.char 'a') <|>
+--   (Adv <$ A.char 'r')
 
-synsetCnt = A.decimal
+-- synsetCnt = A.decimal
 
-pCnt = A.decimal
+-- pCnt = A.decimal
+
+-- data Pos = Noun [PtrSymNoun] | 
+
+{-
+The pointer_symbol s for nouns are:
+
+    !    Antonym 
+    @    Hypernym 
+    @i    Instance Hypernym 
+     ~    Hyponym 
+     ~i    Instance Hyponym 
+    #m    Member holonym 
+    #s    Substance holonym 
+    #p    Part holonym 
+    %m    Member meronym 
+    %s    Substance meronym 
+    %p    Part meronym 
+    =    Attribute 
+    +    Derivationally related form         
+    ;c    Domain of synset - TOPIC 
+    -c    Member of this domain - TOPIC 
+    ;r    Domain of synset - REGION 
+    -r    Member of this domain - REGION 
+    ;u    Domain of synset - USAGE 
+    -u    Member of this domain - USAGE 
+-}
+data PtrSymNoun = AntoN | HyperN | IHyperN | HypoN | IHypoN | MHoloN | SHoloN | PHoloN | MMeroN | SMeroN | PMeroN | AttrN | DerivRelN | DSynTopicN | MDomTopicN | DSynRegionN | MDomRegionN | DSynUsageN | MDomUsageN deriving (Eq, Show)
+
+ptrSymNoun :: A.Parser PtrSymNoun
+ptrSymNoun =
+  (AntoN <$ A.char '!') <|>
+  (HyperN <$ A.char '@') <|>
+  (IHyperN <$ A.string "@i") <|>
+  (HypoN <$ A.char '~') <|>
+  (IHypoN <$ A.string "~i" ) <|>
+  (MHoloN <$ A.string "#m") <|>
+  (SHoloN <$ A.string "#s") <|>
+  (PHoloN <$ A.string "#p") <|>
+  (MMeroN <$ A.string "%m") <|>
+  (SMeroN <$ A.string "%s") <|>
+  (PMeroN <$ A.string "%p") <|>
+  (AttrN <$ A.char '=') <|>
+  (DerivRelN <$ A.char '+') <|>
+  (DSynTopicN <$ A.string ";c") <|>
+  (MDomTopicN <$ A.string "-c") <|>
+  (DSynRegionN <$ A.string ";r") <|>
+  (MDomRegionN <$ A.string "-r") <|>
+  (DSynUsageN <$ A.string ";u") <|>
+  (MDomUsageN <$ A.string "-u") 
+
+{-
+The pointer_symbol s for verbs are:
+
+    !    Antonym 
+    @    Hypernym 
+     ~    Hyponym 
+    *    Entailment 
+    >    Cause 
+    ^    Also see 
+    $    Verb Group 
+    +    Derivationally related form         
+    ;c    Domain of synset - TOPIC 
+    ;r    Domain of synset - REGION 
+    ;u    Domain of synset - USAGE 
+-}  
+data PtrSymVerb = AntoV | HyperV | HypoV | EntailV | CauseV | AlsoSeeV | VerbGroupV | DerivRelV | DSynTopicV | DSynRegionV | DSynUsageV deriving (Eq, Show)
+
+ptrSymVerb :: A.Parser PtrSymVerb
+ptrSymVerb =
+  (AntoV <$ A.char '!') <|>
+  (HyperV <$ A.char '@') <|>
+  (HypoV <$ A.char '~') <|>
+  (EntailV <$ A.char '*') <|>
+  (CauseV <$ A.char '>') <|>
+  (AlsoSeeV <$ A.char '^') <|>
+  (VerbGroupV <$ A.char '$') <|>
+  (DerivRelV <$ A.char '+') <|>
+  (DSynTopicV <$ A.string ";c") <|>
+  (DSynRegionV <$ A.string ";r") <|>
+  (DSynUsageV <$ A.string ";u") 
+
+{-
+The pointer_symbol s for adjectives are:
+
+    !    Antonym 
+    &    Similar to 
+    <    Participle of verb 
+    \    Pertainym (pertains to noun) 
+    =    Attribute 
+    ^    Also see 
+    ;c    Domain of synset - TOPIC 
+    ;r    Domain of synset - REGION 
+    ;u    Domain of synset - USAGE 
+-}
+data PtrSymAdj = AntoAdj | SimilarToAdj | ParticipleAdj | PertainymAdj | AttributeAdj | AlsoSeeAdj | DSynTopicAdj | DSynRegionAdj | DSynUsageAdj deriving (Eq, Show)
+
+ptrSymAdj :: A.Parser PtrSymAdj
+ptrSymAdj =
+  (AntoAdj <$ A.char '!') <|>
+  (SimilarToAdj <$ A.char '&') <|>
+  (ParticipleAdj <$ A.char '<') <|>
+  (PertainymAdj <$ A.char '\\') <|>
+  (AlsoSeeAdj <$ A.char '^') <|>
+  (DSynTopicAdj <$ A.string ";c") <|>
+  (DSynRegionAdj <$ A.string ";r") <|>
+  (DSynUsageAdj <$ A.string ";u")   
+
+{-
+The pointer_symbol s for adverbs are:
+
+    !    Antonym 
+    \    Derived from adjective 
+    ;c    Domain of synset - TOPIC 
+    ;r    Domain of synset - REGION 
+    ;u    Domain of synset - USAGE 
+-}
+data PtrSymAdv = AntoAdv | DerivedFromAdjAdv | DSynTopicAdv | DSynRegionAdv | DSynUsageAdv deriving (Eq, Show)
+
+ptrSymAdv :: A.Parser PtrSymAdv
+ptrSymAdv =
+  (AntoAdv <$ A.char '!') <|>
+  (DerivedFromAdjAdv <$ A.char '\\') <|>
+  (DSynTopicAdv <$ A.string ";c") <|>
+  (DSynRegionAdv <$ A.string ";r") <|>
+  (DSynUsageAdv <$ A.string ";u")   
+
+
 
 {- |
 Each index file begins with several lines containing a copyright notice, version number, and license agreement. These lines all begin with two spaces and the line number so they do not interfere with the binary search algorithm that is used to look up entries in the index files. All other lines are in the following format. In the field descriptions, number always refers to a decimal integer unless otherwise defined.
@@ -81,4 +204,14 @@ tagsense_cnt
 
 synset_offset
     Byte offset in data.pos file of a synset containing lemma . Each synset_offset in the list corresponds to a different sense of lemma in WordNet. synset_offset is an 8 digit, zero-filled decimal integer that can be used with fseek(3) to read a synset from the data file. When passed to read_synset(3WN) along with the syntactic category, a data structure containing the parsed synset is returned. 
+-}
+
+
+
+
+{- |
+-- example from data.noun "compliance" :
+
+0101 + 02548492 v 0101 ! 01182197 n 0101 ~ 01169875 n 0000 | the act of obeying; dutiful or submissive behavior with respect to another person
+
 -}
